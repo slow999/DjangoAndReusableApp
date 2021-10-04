0efe25156e5a6e35eb8a19e1af1e9637a7a52eed
slow999 <slow999@gmail.com>
2021-10-03 23:52:07 -0500

    added polls content

---
+++ django-polls/polls/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    num = 410230
+    percentage = 70
+    days = 30
+    return render(request, 'polls/index.html', context={'number': num, 'percentage': percentage, 'days': days})
