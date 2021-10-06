b6593486fb6506fa94a3e52f82cf7ba9135073e0
slow999 <slow999@gmail.com>
2021-10-05 22:46:59 -0500

    add index.html into templates; removed polls app

--- project_1/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def index(request):
-    num = 410230
-    percentage = 70
-    days = 30
-    return render(request, 'polls/index.html', context={'number': num, 'percentage': percentage, 'days': days})
